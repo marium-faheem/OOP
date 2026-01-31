5417ed516c262f4e95cebda3697c963a3ea6ae22
marium-faheem <marium.faheem7130@gmail.com>
2026-01-31 23:58:14 +0500

    C++

---
+++ default_constructor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Wall { // declare a class
+private:
+double length;
+public:
+// default constructor to initialize variable
+Wall() {
+length = 5.5;
+cout << "Creating a wall." << endl;
+cout << "Length = " << length << endl;
+}
+};
+int main() {
+Wall wall1;
+return 0;
+}
