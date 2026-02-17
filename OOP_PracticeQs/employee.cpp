6c3a21b375fc8e544fc82764be2e91d936438c6c
marium-faheem <marium.faheem7130@gmail.com>
2026-02-17 18:58:35 +0500

    C++

--- OOP_PracticeQs/employee.cpp
+++ OOP_PracticeQs/employee.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
 using namespace std;
-class Employee{
-    int id;
-    int count;
+class Employee {
+    private:
+        int id;
+        int salary;
     public:
-    void setData(void){
-        cout<<"Entrer the id: ";
-        cin>>id;
-    }
-    void getData(void){
-        cout<<"The id of this employee is "<<id;
-    }
+        string name;
+        string position;
+        Employee(int empId, string empName, string empPosition, int empSalary) {
+            id = empId;
+            name = empName;
+            position = empPosition;
+            salary = empSalary;
+        }
+        void displayInfo() {
+            cout << "ID: " << id << endl;
+            cout << "Name: " << name << endl;
+            cout << "Position: " << position << endl;
+            cout << "Salary: $" << salary << endl;
+        }
 };
-
 int main(){
-    Employee marium;
-    marium.setData();
-    marium.getData();
-
+    Employee emp1(1, "John Doe", "Software Engineer", 75000);
+    emp1.displayInfo();
+    Employee emp2(2, "Jane Smith", "Project Manager", 90000);
+    emp2.displayInfo();
     return 0;
 }
